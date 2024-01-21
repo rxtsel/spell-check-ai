bf92711dba39e4165d5681d260e3eeb4903997ad
Cristhian Melo <rxtsel@hotmail.com>
2024-01-21 17:14:16 -0500

    chore: add max tokens

--- src/lib/openai.ts
+++ src/lib/openai.ts
@@ -13,7 +13,7 @@ export const fixOrthograpy = async (ctx: string) => {
         content: `Actúa como una herramienta de corrección ortográfica. Te proporcionaré un fragmento de texto con posibles errores ortográficos, y tu tarea es identificar y corregir esos errores. Si el texto necesita salto de líneas, siéntete en la libertadde ponerlos. Por favor, revisa el siguiente texto que está dentro de  los signos "<>" y no respondas absolutamente nada más que no sea el texto corregido:\n\n<${ctx}>\n\n`
       }
     ],
-    max_tokens: 150,
+    max_tokens: 1150,
     top_p: 1,
     frequency_penalty: 0,
     presence_penalty: 0
