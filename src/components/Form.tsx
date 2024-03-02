aa3ff2fc7b32bfc81865a136748e95818e7c6b61
Cristhian Melo <rxtsel@hotmail.com>
2024-03-01 23:31:38 -0500

    chore: add padding in to bottom in messages container

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -40,6 +40,8 @@ export const Form: React.FC = () => {
   const handleSubmit = () => {
     if (valueTextArea.length === 0) return
 
+    sclollToBottom()
+
     setLoading(true)
 
     answers.push({
@@ -76,8 +78,8 @@ export const Form: React.FC = () => {
       .finally(() => {
         const notificationSound = new Audio('/notification.mp3')
         notificationSound.play()
-        setLoading(false)
         sclollToBottom()
+        setLoading(false)
       })
 
     setValueTextArea('')
@@ -110,74 +112,85 @@ export const Form: React.FC = () => {
   }
 
   return (
-    <section
-      id="messages-section"
-      className="border-t border-l border-r border-zinc-700 w-full max-w-3xl mx-auto rounded-t-3xl h-full flex flex-col shadow p-4 md:p-10 overflow-x-hidden overflow-y-auto relative bottom-0"
-    >
+    <>
       <Toast show={alert} />
-
-      <ul className="h-full flex flex-col space-y-4">
-        <li className="flex items-start space-x-4">
-          <div className="w-12 h-12 text-white border border-purple-500/20 shadow-inner shadow-purple-500 rounded-full p-3 flex items-center justify-center">
-            <Robot />
-          </div>
-          <p className="bg-zinc-900 rounded-xl max-w-full md:max-w-max px-5 py-4">
-            ¡Hola! Soy un bot que te ayudará a corregir tu ortografía. Puedes
-            escribirme y te ayudaré a corregir tus errores.
-          </p>
-        </li>
-
-        {answers.map((answer, index) => (
-          <li
-            key={index}
-            className={`message flex items-start gap-4 ${
-              answer.role === 'user' ? 'self-end' : 'cursor-pointer'
-            }`}
-            {...(answer.role === 'assistant' && {
-              onClick: () => {
-                handleCopyToClipboard(answer.id)
-              }
-            })}
-          >
-            <div
-              className={`w-12 h-12 text-white border shadow-inner rounded-full p-3 flex items-center justify-center ${
-                answer.role === 'user'
-                  ? 'border-sky-500/20 shadow-sky-500 order-2'
-                  : 'border-purple-500/20 shadow-purple-500'
-              }`}
-            >
-              {answer.role === 'assistant' ? <Robot /> : <User />}
-            </div>
-            <p className="bg-zinc-900 rounded-xl w-full max-w-full md:max-w-max paragraph px-5 py-4 text-pretty whitespace-break-spaces">
-              {answer.message}
-            </p>
-          </li>
-        ))}
-      </ul>
-
-      <form
-        onSubmit={handleSubmit}
-        className="fixed bottom-5 left-0 right-0 flex w-full max-w-xs md:max-w-lg mx-auto max-h-14"
-      >
-        <textarea
-          placeholder="Ingresa tu texto aquí"
-          value={valueTextArea}
-          onChange={handleTextareaChange}
-          onKeyDown={handleKeyDown}
-          className="outline-none resize-none bg-zinc-900 h-auto w-full rounded-full text-slate-100 p-4 pr-[70px]"
-        />
-        <button
-          className={`rounded-full bg-purple-500 absolute bottom-0 right-0 h-full px-5 min-w-[86.63px] flex items-center justify-center ${
-            loading ? 'cursor-not-allowed opacity-50' : 'cursor-pointer'
-          }`}
-          disabled={loading}
-          onClick={handleClick}
+      <section className="fixed bottom-0 border-t overflow-hidden border-l border-r border-zinc-700 rounded-t-3xl w-fit h-full max-h-[68vh] lg:min-h-[600px] lg:max-h-[600px] p-4 md:p-10">
+        <div
+          id="messages-section"
+          className="w-full max-w-3xl mx-auto h-full flex flex-col shadow
+        overflow-y-auto relative md:bottom-5"
         >
-          {loading ? <Loader /> : <span className="text-white">Enviar</span>}
-        </button>
-      </form>
+          <ul className="h-full flex-grow flex-shrink flex flex-col space-y-4">
+            <li className="flex items-start space-x-4">
+              <div className="w-12 h-12 text-white border border-purple-500/20 shadow-inner shadow-purple-500 rounded-full p-3 flex items-center justify-center">
+                <Robot />
+              </div>
+              <p className="bg-zinc-900 rounded-xl max-w-full md:max-w-max px-5 py-4">
+                ¡Hola! Soy un bot que te ayudará a{' '}
+                <strong className="font-normal">corregir tu ortografía</strong>.
+                Puedes escribirme y te ayudaré a corregir tus errores.
+              </p>
+            </li>
 
-      <p className="hidden">max length: 150ch</p>
-    </section>
+            {answers.map((answer, index) => (
+              <li
+                key={index}
+                className={`flex items-start gap-4 w-fit h-fit last:pb-20 md:last-pb-0 ${
+                  answer.role === 'user' ? 'self-end' : ''
+                }`}
+              >
+                <div
+                  className={`w-12 h-12 text-white border shadow-inner rounded-full p-3 flex items-center justify-center ${
+                    answer.role === 'user'
+                      ? 'border-sky-500/20 shadow-sky-500 order-2'
+                      : 'border-purple-500/20 shadow-purple-500'
+                  }`}
+                >
+                  {answer.role === 'assistant' ? <Robot /> : <User />}
+                </div>
+                <p
+                  className={`bg-zinc-900 rounded-xl w-full max-w-full md:max-w-max paragraph px-5 py-4 text-pretty whitespace-break-spaces ${
+                    answer.role !== 'user' ? 'cursor-pointer' : ''
+                  }`}
+                  {...(answer.role === 'assistant' && {
+                    onClick: () => {
+                      handleCopyToClipboard(answer.id)
+                    }
+                  })}
+                >
+                  {answer.message}
+                </p>
+              </li>
+            ))}
+          </ul>
+
+          <form
+            onSubmit={handleSubmit}
+            className="fixed bottom-5 left-0 right-0 flex w-full max-w-xs md:max-w-lg mx-auto max-h-14"
+          >
+            <textarea
+              placeholder="Ingresa tu texto aquí"
+              value={valueTextArea}
+              onChange={handleTextareaChange}
+              onKeyDown={handleKeyDown}
+              className="outline-none resize-none bg-zinc-900 h-auto w-full rounded-full text-slate-100 p-4 pr-[70px]"
+            />
+            <button
+              className={`rounded-full bg-purple-500 absolute bottom-0 right-0 h-full px-5 min-w-[86.63px] flex items-center justify-center ${
+                loading ? 'cursor-not-allowed opacity-50' : 'cursor-pointer'
+              }`}
+              disabled={loading}
+              onClick={handleClick}
+            >
+              {loading ? (
+                <Loader />
+              ) : (
+                <span className="text-white">Enviar</span>
+              )}
+            </button>
+          </form>
+        </div>
+      </section>
+    </>
   )
 }
